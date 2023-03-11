76179fdac20e6e0066d73eec87547336bde4eab7
Carsten Knoll <carsten.knoll@posteo.de>
2023-03-11 13:16:37 +0100

    fix some initial bugs

--- src/fimf/core.py
+++ src/fimf/core.py
@@ -13,6 +13,7 @@ class CustomApp(App):
     CSS_PATH = "fimf.css"
     BINDINGS = [
         ("f1", "help", "show help"),
+        ("f3", "do_search", "search"),
         ("escape", "cmd_select", "command mode"),
         ("p", "plain_text_select", "mode: plain text"),
         ("e", "escape_seq_select", "mode: escape-sequences"),
@@ -26,9 +27,10 @@ class CustomApp(App):
         self.input_search = Input(placeholder="search pattern", id="in_search")
         self.input_replace = Input(placeholder="replace pattern", id="in_replace")
 
-        self.button_search = Button("Search", id="btn_search", variant="primary")
+        self.button_search = Button("Search (F3)", id="btn_search", variant="primary")
         self.button_replace = Button("Replace All", id="btn_replace", variant="warning")
-        self.button_cancel = Button("Cancel", id="btn_cancel", variant="error")
+        self.button_quit = Button("Quit (CTRL+C)", id="btn_quit", variant="error")
+        self.label_results = Label("Results", id="lb_results")
         self.results = TextLog(markup=True, classes="results")
 
         yield self.intro
@@ -40,25 +42,21 @@ class CustomApp(App):
             with Horizontal(id="cntn_buttons"):
                 yield self.button_search
                 yield self.button_replace
-                yield self.button_cancel
+                yield self.button_quit
 
+            yield self.results
             yield self.results
         yield Footer()
 
     def on_mount(self) -> None:
-        self.input_files.focus()
-        self.results.write("**test1**")
-        self.results.write("[#9944aa]test2[/#9944aa]" + "\nxy" * 23)
-
-    def on_input_changed(self, message: Input.Changed) -> None:
-        print(message.input)
-        self.results.write(message.value)
+        # self.input_files.focus()
+        self.input_search.focus()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
 
         if event.button.id == "btn_search":
-            self.do_search()
-        elif event.button.id == "btn_cancel":
+            self.action_do_search()
+        elif event.button.id == "btn_quit":
             self.exit(str(event.button))
         else:
             # !! unexpected -> raise exception
@@ -75,18 +73,36 @@ class CustomApp(App):
     def action_cmd_select(self) -> None:
         self.screen.set_focus(None)
 
-    def do_search(self):
+    def action_do_search(self):
         file_pattern = self.input_files.value
         search_pattern = self.input_search.value
         replace_pattern = self.input_replace.value
 
+        # for testing
+        if not file_pattern:
+            file_pattern = "*.njk"
+
+        self.results.clear()
+        if not search_pattern:
+            self.results.write("error: empty search pattern")
+            return
+
+        try:
+            search_pattern = re.compile(search_pattern)
+        except re.error as ex:
+            self.results.write(f"regex error: {ex}")
+            return
+
+
         results = find_pattern("./", search_pattern, file_pattern)
         indent = " " * 4
         for path, matches in results.items():
+            if not matches:
+                continue
             self.results.write(f"{path} ({len(matches)})")
-            for line_number, match in matches:
-                self.results.write(f"{indent}{line_number:03d} {match}")
-            self.results.write("\n")
+            for line_number, match in matches.items():
+                self.results.write(f"{indent}{line_number:03d}: {match[0]}")
+            self.results.write("\n"*2)
 
 
 class HelpScreen(Screen):
@@ -94,7 +110,7 @@ class HelpScreen(Screen):
         yield Grid(
             Static("This modal dialog will be the help screen in the future", id="question"),
             Button("Quit", variant="error", id="quit"),
-            Button("Cancel", variant="primary", id="cancel"),
+            Button("Cancel", variant="primary", id="quit"),
             id="dialog",
         )
 
@@ -105,11 +121,11 @@ class HelpScreen(Screen):
             self.app.pop_screen()
 
 
-def find_matches(filename, pattern):
+def find_matches(filename, compiled_pattern):
     matches = {}
     with open(filename, "r") as f:
         for i, line in enumerate(f, start=1):
-            for match in re.finditer(pattern, line):
+            for match in re.finditer(compiled_pattern, line):
                 if i not in matches:
                     matches[i] = [match.group()]
                 else:
