99252868068973b8d4710d5753b5b59f5ac9015a
Carsten Knoll <carsten.knoll@posteo.de>
2023-03-15 23:37:41 +0100

    script.py: allow running in debug mode via the usual command

--- src/fimf/script.py
+++ src/fimf/script.py
@@ -10,6 +10,17 @@ activate_ips_on_exception()
 
 
 def main():
+    # hacky version of the debug mode
+
+    from textual.cli.cli import run
+    import sys
+
+
+    sys.argv = f"textual run --dev {core.__file__}".split()
+    sys.exit(run())
+
+
+def main1():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--helpq", help=f"show help", action="store_true")
