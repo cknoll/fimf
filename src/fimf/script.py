cf1323c933ccba655430283376c7c08cc3cb6314
Carsten Knoll <carsten.knoll@posteo.de>
2023-03-24 15:10:41 +0100

    switch off debug mode

--- src/fimf/script.py
+++ src/fimf/script.py
@@ -2,29 +2,17 @@
 Command line interface for autobrowser package
 """
 
-import argparse
-from ipydex import IPS, activate_ips_on_exception
 from . import core
-
-activate_ips_on_exception()
 
 
 def main():
-    # hacky version of the debug mode
+    if 0:
+        from textual.cli.cli import run
+        import sys
 
-    from textual.cli.cli import run
-    import sys
+        sys.argv = f"textual run --dev {core.__file__}".split()
+        sys.exit(run())
+    else:
 
-    sys.argv = f"textual run --dev {core.__file__}".split()
-    sys.exit(run())
-
-
-def main1():
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--helpq", help=f"show help", action="store_true")
-
-    args = parser.parse_args()
-
-    # in the future there could be some commandline arguments here
-    core.main()
+        # in the future there could be some commandline arguments here
+        core.main()
